5ddb7f1ef9f7e2329105ccfbd614835e02eaa36f
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2021-11-29 15:35:58 -0300

    Correçao

--- Desafio_final/desafio_final.rb
+++ Desafio_final/desafio_final.rb
@@ -1,59 +1,57 @@
 require 'net/http'
 
-# nome dos métodos em inglês
-# nome das classes em inglês
-# nome das variáveis em inglês
+# nome dos métodos em inglês >Blz
+# nome das classes em inglês >Blz
+# nome das variáveis em inglês >Blz
 
-class ColetaDados
-  def coletar
+class CollectData
+def collect
     puts "- - - -Olá! Vamos conferir o CEP - - - -"
     puts ''
     puts 'Digite o CPF: '
-    cep = gets.chomp # não precisa atribuir a variável, pois não está sendo utilizada
+    gets.chomp # não precisa atribuir a variável, pois não está sendo utilizada >Blz
   end
 end
 
-class ConectaApi
-  def conecta
-    coletaDados = ColetaDados.new
-    https = Net::HTTP.new('api.invertexto.com', 443) # definir os valores em constantes
+class ConnectApi
+  def connect
+    collectData = CollectData.new
+    tokenApi = '153|IizI3MqKmi7Hn4akkpOIvXweFSnRZPIH'
+    url = 'api.invertexto.com'
+    port = '443'
+    https = Net::HTTP.new(url,port) # definir os valores em constantes
     https.use_ssl = true
-    tokenApi = "153|IizI3MqKmi7Hn4akkpOIvXweFSnRZPIH" # utilizar apóstrofo ao invés de aspas, pois não há concatenação de strings
-    response = https.get("/v1/cep/#{coletaDados.coletar}?token=#{tokenApi}")
-    @@code = response.code # por que aqui ser uma variável de classe e não de instância?
-    @@message = response.message
-    @@body = response.body
+    $response = https.get("/v1/cep/#{collectData.collect}?token=#{tokenApi}")
+    $response.code # por que aqui ser uma variável de classe e não de instância? Para que ela possa ser compartilhada com outra classe
+    $response.message
+    $response.body
   end
 end
 
-class Escrever < ConectaApi # Geralmente nomes de classes são substantivos
-  # por que Escrever está herdando a classe ConectaApi?
-  #
+class Archive # Geralmente nomes de classes são substantivos
+  # por que Escrever está herdando a classe ConectaApi? >Eu precisava usar as variáveis @@code, @@message, @@body, mas eu já estou instanciando a classe e chamando o método então não teria necessidade
   #por que esse código não está num construtor ou em outro método?
-  conectaApi = ConectaApi.new
-  conectaApi.conecta
-  def escreverTerminal
-    puts @@code
-    puts @@message
-    puts @@body
+  
+  def write_terminal
+    connectApi = ConnectApi.new
+    puts connectApi.connect
   end
 
-  def escreverArquivo # convenção ruby é ter classes com camel case (escrever_arquivo)
-    # será que um create_file não seria mais interessante? O que acha?
+  def write_archive # convenção ruby é ter classes com camel case (escrever_arquivo) > Blz
+    # será que um create_file não seria mais interessante? O que acha? >Você quer dizer um método para criar o arquivo e depois um para escrever?
     time = Time.now
     date = time.strftime('%d/%m/%y')
-    day = time.day
     hour = time.strftime('%H:%M')
-    File.open('Endereco_completo.txt','a') do |line| # nome do arquivo poderia estar em uma constante de classe
+    @@archive = 'Endereco_completo.txt'
+    File.open(@@archive,'a') do |line| # nome do arquivo poderia estar em uma constante de classe >Blz
       line.puts('')
       line.puts("Consulta feita em #{date} às #{hour}")
-      line.puts(@@body)
+      line.puts($response.body)
       line.puts('- - - - - Fim da consulta - - - -')
     end
   end
-
 end
 
-escrever = Escrever.new
-escrever.escreverTerminal
-escrever.escreverArquivo
+archive = Archive.new
+archive.write_terminal
+archive.write_archive
